903b8436fa8d948a6b839b1be2d2f7abdcdf66ef
Hayati BAHAR <hayati.458@hotmail.com>
2024-03-21 18:04:26 +0300

    add colors

--- app/src/main/java/com/hayatibahar/composeplayground/ui/theme/Color.kt
+++ app/src/main/java/com/hayatibahar/composeplayground/ui/theme/Color.kt
@@ -9,3 +9,49 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val Orange400 = Color(0xffFFA726)
+val Blue400 = Color(0xff42A5F5)
+val Pink400 = Color(0xffEC407A)
+val Green400 = Color(0xff66BB6A)
+val Red400 = Color(0xffEF5350)
+val Yellow400 = Color(0xffFFEE58)
+val Purple400 = Color(0xffAB47BC)
+val Brown400 = Color(0xff8D6E63)
+val BlueGrey400 = Color(0xff78909C)
+
+val backgroundColor = Color(0xffECEFF1)
+
+
+val DefaultListColor = Color(0xff00BCD4)
+val LayoutListColor = Color(0xffFFEB3B)
+val StateListColor = Color(0xffE91E63)
+val GestureListColor = Color(0xff8BC34A)
+val GraphicsListColor = Color(0xffFF9800)
+
+val SentMessageColor = Color(0xffE7FFDB)
+val SentQuoteColor = Color(0xffDEF6D3)
+val ReceivedQuoteColor = Color(0xffECEFF1)
+val QuoteTextColor = Color(0xff757575)
+val RecipientAltNameColor = Color(0xff757575)
+
+val gradientColors = listOf(
+    Color.Red,
+    Color.Magenta,
+    Color.Blue,
+    Color.Cyan,
+    Color.Green,
+    Color.Yellow,
+    Color.Red
+)
+
+val gradientColorsReversed = listOf(
+    Color.Red,
+    Color.Yellow,
+    Color.Green,
+    Color.Cyan,
+    Color.Blue,
+    Color.Magenta,
+    Color.Red
+)
